9e64c186145f474ee505e9cf98664c89e1b214e9
mgogte <mugdha.2608@gmail.com>
2016-09-17 03:10:02 -0400

    Add files via upload

---
+++ Splitwise.py
@@ -0,0 +1,18 @@
+# Splitwise
+
+n,k = raw_input().split() #n items ordered, anna did not eat kth item
+arr = raw_input().split() #prices of n items
+anna_paid = raw_input().split() # anna paid this amount
+anna_paid = int(anna_paid[0])
+sum = 0 #stores what she should have paid
+k = int(k)
+for a in arr:
+    sum = sum + int(a)
+sum = (sum - int(arr[k]))/2
+
+
+
+if (anna_paid - sum) == 0:
+    print 'bon appetite'
+else:
+    print anna_paid - sum
